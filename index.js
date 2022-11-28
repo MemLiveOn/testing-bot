8fbfa5c345e710ffe614b448f3a0443d0ad8bfaf
TGB <greatbeyond67@gmail.com>
2022-11-28 06:42:12 -0500

    Adding image welcome thingy

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const Discord = require("discord.js")
 const { GatewayIntentBits } = require("discord.js")
 require("dotenv").config()
+const generateImage = require ("./generateImage")
 
 const TOKEN = process.env.TOKEN
 
 const client = new Discord.Client({
 	intents: [
 		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers
 	]
 })
 
 client.on("ready", () => {
 	console.log(`Logged in as ${client.user.tag}`)
+})
+
+
+const welcomeChannelID = "1046665525939404901"
+
+
+client.on("guildMemberAdd", async (member) => {
+	const img = await generateImage(member)
+	member.guild.channels.cache.get(welcomeChannelID).send({
+		content: `<@${member.id}> Welcome to the server!`,
+		files: [img]
+	})
 })
 
 client.login(TOKEN)
